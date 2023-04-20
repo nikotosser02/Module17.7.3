2d5f8404bc412f305dae61d884f975ccf9202097
nikotosser02 <128023327+nikotosser02@users.noreply.github.com>
2023-04-20 17:19:22 +0300

    add round

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ money = float(input ("Введите желаемую сумму вклада: "
 
 list_per_cent = {}
 for a in per_cent:
-    list_per_cent[a]=money * (float(per_cent[a]/100)) #перемножаем вклад на проценты
+    list_per_cent[a]=round(money * (float(per_cent[a]/100)), 2) #перемножаем вклад на проценты
 best_bank_max_pers = max(list_per_cent.values()) #для поиска максимального
 
 print("Проценты доходности по банкам: ", per_cent)
 print("Доход составит по вкладам в банках: ", list_per_cent)
-print("Максимальный доход: ", best_bank_max_pers)
+print("Максимальный доход: ", round(best_bank_max_pers, 2))
