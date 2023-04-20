9f2207c3a291e43d14b6913f7b9df220b1501103
nikotosser02 <128023327+nikotosser02@users.noreply.github.com>
2023-04-20 15:45:03 +0300

    add percent value/100

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ money = float(input ("Введите желаемую сумму вклада: "
 
 list_per_cent = {}
 for a in per_cent:
-    # max_pers1 = \
-    list_per_cent[a]=money * float(per_cent[a]) #перемножаем вклад на проценты
-    # if max_pers<max_pers1:
-    #     max_pers = max_pers1
-    #     best_bank = max_pers1
-    #     print("<fyr>: ", best_bank)
+    list_per_cent[a]=money * (float(per_cent[a]/100)) #перемножаем вклад на проценты
 best_bank_max_pers = max(list_per_cent.values()) #для поиска максимального
 
 print("Проценты доходности по банкам: ", per_cent)
